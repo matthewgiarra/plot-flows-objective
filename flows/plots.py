c410116e8aea505cb21882910954de3bbc1979a5
Kunlong <li1503@purdue.edu>
2016-06-08 01:36:01 -0400

    Update plots.py
    Add a "def VectorField" to plot velocity field

--- flows/plots.py
+++ flows/plots.py
@@ -1,6 +1,8 @@
 # Plotting
 import matplotlib as mpl
-
+import numpy as np
+from .velocities import *
+from .particles import *
 # Select the graphics back end
 # Apparently this has to happen 
 # before importing pyplot.
@@ -8,7 +10,48 @@ mpl.use('TkAgg');
 
 # Import pyplot
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
+fig = plt.figure()
+ax = fig.add_subplot(111, projection = '3d')
+
+
+# This function plots the vector field of velocities
+# Todo: Convert list to array
+# Get Correct velocity
+def VectorField(ax, ParticleField = None):
+    if ParticleField is not None:
+	
+        # Get all velocities
+        x, y, z, u, v, w = ParticleField.GetVelocity()
+       
+		
+        x_new = np.asarray(x)
+        y_new = np.asarray(y)
+        z_new = np.zeros(x_new.shape[0])
+		
+        u_new = np.asarray(u)
+        v_new = np.asarray(v)
+        w_new = np.zeros(u_new.shape[0])
+        print(x_new.shape[0])
+        print(y_new.shape[0])
+        #print(z)
+        print(u_new.shape[0])
+        print(v_new.shape[0])
+        #print(w)
+        # Meshgrid the vectors
+        x_rec, y_rec, z_rec, u_rec, v_rec, w_rec = np.meshgrid(x_new, y_new,z_new, u_new, v_new, w_new)
+		
+        print(x_rec.shape[0])
+        print(y_rec.shape[0])
+		
+        print(u_rec.shape[0])
+        print(v_rec.shape[0])
+		
+        # Plot Vector field of velocity
+        ax.quiver(x_rec, y_rec, z_rec, u_rec, v_rec, w_rec, length = 0.01)
+
+        plt.show()
 
 
 # This function plots streaklines.
@@ -17,7 +60,7 @@ def StreakPlot(ax, ParticleField = None):
         
         # Get all the streakline
         x_streak, y_streak, z_streak, d_streak = ParticleField.GetStreaklines();
-        
+     
         # Number of streaklines
         n_streaks = len(x_streak);
         
@@ -28,7 +71,10 @@ def StreakPlot(ax, ParticleField = None):
             npoints = len(x_streak[n]);
 
             # Plot this streak line
-            ax.plot(x_streak[n], y_streak[n], '.');
+            #ax.plot(x_streak[n], y_streak[n], '.');
+            ax.plot(x_streak[n], y_streak[n], z_streak[n], '.');
+            #ax.scatter( x_streak, y_streak, z_streak, c = 'r', marker = 'o')
+            #plt.show()
             
             
 def PathlinePlot(ax, ParticleField = None):
